d93d7ac432b9866dccd87390d249476195945ae2
Alexandru Jora <alexandru@jora.ca>
2020-10-22 11:47:10 -0400

    __init__.py: expose sub-packages don't expose moco because it is a major offender of layer violation

--- spinalcordtoolbox/__init__.py
+++ spinalcordtoolbox/__init__.py
@@ -2,3 +2,16 @@
 
 # convenience and backwards compat
 from .utils import __version__, __sct_dir__, __data_dir__, __deepseg_dir__
+from . import aggregate_slicewise
+from . import cropping
+from . import download
+from . import image
+from . import labels
+from . import math
+from . import metadata
+# from . import moco # FIXME
+from . import process_seg
+from . import resampling
+from . import straightening
+from . import template
+from . import types

53a306ae6c2d087bb86b085b85d36566c4a4cc46
Alexandru Jora <alexandru@jora.ca>
2020-10-23 08:39:10 -0400

    Forgot flattening.py + add some comments

--- spinalcordtoolbox/__init__.py
+++ spinalcordtoolbox/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-# convenience and backwards compat
+# explicitely expose __version__ and friends in top-level package
 from .utils import __version__, __sct_dir__, __data_dir__, __deepseg_dir__
+
 from . import aggregate_slicewise
 from . import cropping
 from . import download
@@ -10,9 +11,11 @@ from . import labels
 from . import math
 from . import metadata
 from . import flattening
-# from . import moco # FIXME
 from . import process_seg
 from . import resampling
-from . import straightening
+from . import straighteningq
 from . import template
 from . import types
+
+# FIXME
+# from . import moco
